33631f0fbe1a758a70e06c670c160f320ada8e88
logiclogue <jordanpaullord@hotmail.co.uk>
2019-08-15 14:56:39 +0100

    Rewriting split

--- src/Helpers/RoundRobinHelpers.ml
+++ src/Helpers/RoundRobinHelpers.ml
@@ -1,9 +1,18 @@
-let split xs =
+(*let split xs =
     match xs with
     | [] -> [], []
     | [x] -> [x], []
     | [x; y] -> [x], [y]
     | [x; y; z] -> [x; y], [z]
+    | [x; y; z; w] -> [x; y], [z; w]*)
+
+let split xs =
+    let length = List.length xs in
+    let half = int_of_float (ceil (float_of_int length /. float_of_int 2)) in
+
+    let () = Js.log (List.take half xs) in
+
+    [], []
 
 let equalise xys =
     [], []
