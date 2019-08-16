5bb960d47ffee5ece71fa4505d83868770bdc0c5
logiclogue <jordanpaullord@hotmail.co.uk>
2019-08-16 06:07:36 +0100

    Writes split

--- src/Helpers/RoundRobinHelpers.ml
+++ src/Helpers/RoundRobinHelpers.ml
@@ -6,13 +6,27 @@
     | [x; y; z] -> [x; y], [z]
     | [x; y; z; w] -> [x; y], [z; w]*)
 
+let rec take n xs =
+    if n <= 0 then
+        []
+    else
+        match xs with
+        | x :: xs -> x :: (take (n - 1) xs)
+        | []      -> []
+
+let rec drop n xs =
+    if n <= 0 then
+        xs
+    else
+        match xs with
+        | x :: xs -> drop (n - 1) xs
+        | []      -> []
+
 let split xs =
     let length = List.length xs in
     let half = int_of_float (ceil (float_of_int length /. float_of_int 2)) in
 
-    let () = Js.log (List.take half xs) in
-
-    [], []
+    take half xs, drop half xs
 
 let equalise xys =
     [], []
