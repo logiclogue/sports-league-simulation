79dd0b6e3d3751c4017832b18dd9041aac3cb230
logiclogue <jordanpaullord@hotmail.co.uk>
2019-12-27 23:39:15 +0000

    Updating the domain

--- journal.md
+++ journal.md
@@ -1,3 +1,7 @@
 # 26 December 2019
 
-Using the requirements, the basic domain must be sketched out. (TODO)
+Using the requirements, the basic domain must be sketched out.
+
+# 27 December 2019
+
+Sketching out the domain in `Domain.ml`.
--- src/Domain.ml
+++ src/Domain.ml
@@ -1,1 +1,17 @@
+type rating = int
+
+type rating_change = int
+
 type team_name = string
+
+type team = team_name * rating
+
+type fixture = team * team
+
+type get_rating = team -> rating
+
+type result = Win | Draw | Loss
+
+type outcome = result * rating_change
+
+type get_result = teams -> fixture -> result
