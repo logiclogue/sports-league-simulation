50ede3bd48b40a0fbeb4168a721e663f84ff7326
logiclogue <jordanpaullord@hotmail.co.uk>
2020-01-07 23:58:07 +0000

    Blah

--- src/main.ml
+++ src/main.ml
@@ -6,6 +6,7 @@ let () =
         |> OptionHelpers.bind (fun dict -> Js.Dict.get dict "name")
         |> OptionHelpers.bind Js.Json.decodeString in
 
+
     match json_opt with
     | Some name -> Js.log name
     | None      -> Js.log "Failed to parse"
